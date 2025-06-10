41f43227c221f605cd7aeb80443ae178e860d1ee
JustinC1604 <s10266690@connect.np.edu.sg>
2025-06-10 23:56:01 +0800

    door commit

---
+++ Assets/scripts/Door.cs
@@ -0,0 +1,25 @@
+/*
+* Author:
+* Date:
+* Description:
+*/
+
+
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+   
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
